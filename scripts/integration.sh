fa9c33f5ceee2fff9bb217fea2db73adde37f264
Tyler Phelan <tphelan@pivotal.io>
2019-03-06 09:59:41 -0500

    Initial commit
    - Add repo files
    - Add scripts and vendor code
    
    [#163528038]
    
    Co-authored-by: Matt McNew <mmcnew@pivotal.io>

---
+++ scripts/integration.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export PATH="$DIR/../.bin":$PATH
+go run -mod=vendor "$DIR/cmd_router.go" integration "$@"
