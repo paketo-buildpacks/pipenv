968be5ec6446f4f2b2ddeea64b8d0ce6ba76599d
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2019-03-19 14:52:13 +0000

    Update CNB tools

--- scripts/all-tests.sh
+++ scripts/all-tests.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-set -e
+cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+./scripts/unit.sh && ./scripts/integration.sh
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-"$DIR/unit.sh" "$@" && "$DIR/integration.sh" "$@"
