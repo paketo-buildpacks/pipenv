2092c7119281f2a201a686bd13955a9dd44849c9
Ryan Moran <rmoran@pivotal.io>
2019-09-11 14:18:50 -0400

    Rename python-cnb to python-runtime-cnb
    [#167023338]

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -27,7 +27,7 @@ func TestIntegration(t *testing.T) {
 	Expect(err).ToNot(HaveOccurred())
 	defer dagger.DeleteBuildpack(pipenvURI)
 
-	pythonURI, err = dagger.GetLatestBuildpack("python-cnb")
+	pythonURI, err = dagger.GetLatestBuildpack("python-runtime-cnb")
 	Expect(err).ToNot(HaveOccurred())
 	defer dagger.DeleteBuildpack(pythonURI)
 
