78996ae7636975ec30edd12798d93092d955967e
Rob Dimsdale-Zucker <rdimsdale@vmware.com>
2022-01-12 14:17:04 -0500

    Increase default timeout
    Signed-off-by: Timothy Hitchener <thitchener@pivotal.io>

--- integration/init_test.go
+++ integration/init_test.go
@@ -106,7 +106,7 @@ func TestIntegration(t *testing.T) {
 		Execute(settings.Config.BuildPlan)
 	Expect(err).NotTo(HaveOccurred())
 
-	SetDefaultEventuallyTimeout(10 * time.Second)
+	SetDefaultEventuallyTimeout(20 * time.Second)
 
 	suite := spec.New("Integration", spec.Report(report.Terminal{}))
 	suite("Default", testDefault, spec.Parallel())
